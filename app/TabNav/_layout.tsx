bf9e230d1b0ee46d3db43e81516496ae71adbd8f
Enzo0708 <enzobiason7@gmail.com>
2024-04-04 15:20:04 -0300

    up

--- app/TabNav/_layout.tsx
+++ app/TabNav/_layout.tsx
@@ -4,7 +4,12 @@ export default function layout() {
     return (
         <Tabs>
             <Tabs.Screen name="Scanner" />
-            <Tabs.Screen name="DrawerNav" options={{headerShown: false}} />
+            <Tabs.Screen name="DrawerNav" options={{
+                headerShown: false,
+                tabBarStyle: {
+                    backgroundColor: "#ff0000",
+                },
+                }} />
         </Tabs>
     )
 }
