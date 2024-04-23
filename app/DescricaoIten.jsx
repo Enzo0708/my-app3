0c434261648b914048bcba0f77ebe2d4dfe0be7e
Enzo0708 <enzobiason7@gmail.com>
2024-04-23 20:01:20 -0300

    up

--- app/DescricaoIten.jsx
+++ app/DescricaoIten.jsx
@@ -1,57 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import { ScrollView, StyleSheet, View } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-import Objetos2 from '../../components/objeto2';
-import Footer from '../components/footer';
-import NavbarCadastro from '../../components/navbarCadastroItens';
-import ButtonPlus from '../../components/buttom1'; // Importe o ButtonPlus aqui
-import { MaterialIcons } from '@expo/vector-icons';
-import { FontAwesome } from '@expo/vector-icons';
+import React from "react";
+import { View, StyleSheet, Text} from "react-native";
+import {MaterialCommunityIcons } from '@expo/vector-icons';
+import { Link } from "expo-router";
 
 
-export default function DescItens() {
-  return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <NavbarCadastro
-        text='Descrição do item'
-        color="#FF0000"
-        colorText='#FFFFFF'
-        icons={{
-          icon1: <Ionicons name="arrow-back" size={30} color="white" />,
-          icon2: <Ionicons name="reload-sharp" size={30} color="white" />
-        }}
-      />
-      <ScrollView style={styles.margin}>
-      <Objetos2 text="957689" color="#FFFFFF" tittle='Nº do Inventário:'/>
-      <Objetos2 text="CADEIRA GIRATÓRIOA" color="#FFFFFF" tittle='Descrição:'/>
-      <Objetos2 text="SALA C13" color="#FFFFFF" tittle='Local:'/>
-      <Objetos2 text="CARLOS" color="#FFFFFF" tittle='Responsável:'/>
-      <Objetos2 text="05/03/2024" color="#FFFFFF" tittle='Data de Registro:'/>
+const Descricao = () => {
+    return(
+        <View style={styles.container}>
+            <View style={styles.corpo}>
+                <Text style={styles.titulo}>N° do Inventario:</Text>
+                <Text style={styles.item}> 957689 </Text>
+                <Text style={styles.titulo}>Descrição:</Text>
+                <Text style={styles.item}> CADEIRA GIRATÓRIA </Text>
+                <Text style={styles.titulo}>Local:</Text>
+                <Text style={styles.item}> SALA C13 </Text>
+                <Text style={styles.titulo}>Responsável:</Text>
+                <Text style={styles.item}> CARLOS </Text>
+                <Text style={styles.titulo}>Data de Registro:</Text>
+                <Text style={styles.item}> 05/03/2024 </Text>
+            </View>
 
-
-     
-
-      </ScrollView>
-      <Footer color='#FFFFFF' iconNav={{
-        icon1nav: <FontAwesome name="list-ol" size={30} color="#595959" />,
-        icon2nav: <MaterialIcons name="flip-camera-ios" size={30} color="#595959" />
-      }}/>
-      <ButtonPlus iconAdd={{ // Mova o ButtonPlus para dentro do View principal
-        iconButtom: <MaterialCommunityIcons name="pencil-circle" size={50} color="red" />
-      }}/>
-
-    </View>
-    
-  );
-}
+            <View style={styles.addButtonContainer}>
+                <Link href="/Editar" asChild>
+                    <MaterialCommunityIcons name="pencil-circle" size={60} color="red" />
+                </Link>
+            </View>
+        </View>
+    );
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  margin: {
-    marginTop: 60,
-  }
+    container: {
+        flex: 1,
+    },
+    text: {
+        color: "#ffff",
+        fontSize: 16,
+    },
+    corpo: {
+        flex: 1,
+        marginTop: 50,
+    },
+    addButtonContainer: {
+        position: "absolute",
+        bottom: 20,
+        right: 0,
+        margin: 20,
+        marginBottom: 50,
+        height: 60,
+    },
+    titulo : {
+        fontSize: 20,
+        fontWeight: '500',
+        color: 'black',
+        alignContent: 'center',
+        marginLeft: 25,
+        marginTop: 20,
+    },
+    item: {
+        fontSize: 15,
+        fontWeight: '500',
+        color: 'lightgray',
+        alignContent: 'center',
+        marginLeft: 25,
+        marginBottom: 20,
+    },
 });
+
+export default Descricao;
